0dda088e8741d84a612286526a83081e2a6ad856
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2021-12-26 10:56:27 +0530

    Time: 60 ms (49.79%), Space: 27.9 MB (13.53%) - LeetHub

---
+++ wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n=s.length(), m=p.length();
+        vector<vector<int>>dp(n+1, vector<int>(m+1, false));
+        dp[0][0]=true;
+        for(int i=0;i<m && p[i]=='*';i++){
+            dp[0][i+1]=true;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1;j<=m;j++){
+                if(p[j-1]=='*'){
+                    dp[i][j]=dp[i][j-1]||dp[i-1][j];
+                }else{
+                    dp[i][j]=(s[i-1]==p[j-1] || p[j-1]=='?') && dp[i-1][j-1];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
