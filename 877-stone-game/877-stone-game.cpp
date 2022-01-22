710a19d6ba899fc507aaa8cc5075817975d7a0b4
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-01-22 10:49:17 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (72.61%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int bob=0, alice=0;
-    map<pair<int, int>, int>mp;
-    int stoneGameCalc(vector<int>& piles, int si, int ei, bool turn){
-        if(ei<si)
-            return 0;
-        if(mp[{si, ei}]){
-            return mp[{si, ei}];
-        }
-        if(turn){
-            int a1=stoneGameCalc(piles, si+1, ei, false)+piles[si];
-            int a2=stoneGameCalc(piles, si, ei-1, false)+piles[ei];
-            alice+=max(a1, a2);
-            return mp[{si, ei}]=max(a1, a2);
-        }else{
-            int b1=stoneGameCalc(piles, si+1, ei, true)-piles[si];
-            int b2=stoneGameCalc(piles, si, ei-1, true)-piles[ei];
-            bob+=min(b1, b2);
-            return mp[{si, ei}]=min(b1, b2);
-        }
-    }
+    // int bob=0, alice=0;
+    // map<pair<int, int>, int>mp;
+    // int stoneGameCalc(vector<int>& piles, int si, int ei, bool turn){
+    //     if(ei<si)
+    //         return 0;
+    //     if(mp[{si, ei}]){
+    //         return mp[{si, ei}];
+    //     }
+    //     if(turn){
+    //         int a1=stoneGameCalc(piles, si+1, ei, false)+piles[si];
+    //         int a2=stoneGameCalc(piles, si, ei-1, false)+piles[ei];
+    //         alice+=max(a1, a2);
+    //         return mp[{si, ei}]=max(a1, a2);
+    //     }else{
+    //         int b1=stoneGameCalc(piles, si+1, ei, true)-piles[si];
+    //         int b2=stoneGameCalc(piles, si, ei-1, true)-piles[ei];
+    //         bob+=min(b1, b2);
+    //         return mp[{si, ei}]=min(b1, b2);
+    //     }
+    // }
     bool stoneGame(vector<int>& piles) {
-        bool turn=true;
-        stoneGameCalc(piles, 0, piles.size()-1, turn);
-        return alice>bob;
+        // bool turn=true;
+        // stoneGameCalc(piles, 0, piles.size()-1, turn);
+        // return alice>bob;
+        return true;
     }
 };
