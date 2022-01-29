855de6558c42724bdeecd48f4e225a9e19d61b4a
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-01-29 22:56:43 +0530

    Time: 256 ms (19.01%), Space: 68.3 MB (20.46%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        vector<int>leftMin(n), rightMax(n);
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            if(st.empty()){
+                st.push(i);
+                leftMin[i]=0;
+            }else{
+                if(heights[st.top()]>=heights[i]){
+                    while(!st.empty() && heights[st.top()]>=heights[i]){
+                        st.pop();
+                    }
+                    if(st.empty()){
+                        leftMin[i]=0;
+                    }else{
+                        leftMin[i]=st.top()+1;
+                    }
+                    st.push(i);
+                }else{
+                    leftMin[i]=st.top()+1;
+                    st.push(i);
+                }
+            }
+        }
+        // for(int i=0;i<n;i++){
+        //     cout<<leftMin[i]<<" ";
+        // }
+        // cout<<"\n";
+        while(!st.empty()){
+            st.pop();
+        }
+        for(int i=n-1;i>=0;i--){
+            if(st.empty()){
+                st.push(i);
+                rightMax[i]=n-1;
+            }else{
+                if(heights[st.top()]>heights[i]){
+                    while(!st.empty() && heights[st.top()]>=heights[i]){
+                        st.pop();
+                    }
+                    if(st.empty()){
+                        rightMax[i]=n-1;
+                    }else{
+                        rightMax[i]=st.top()-1;
+                    }
+                    st.push(i);
+                }else{
+                    rightMax[i]=st.top()-1;
+                    st.push(i);
+                }
+            }
+        }
+        
+        // for(int i=0;i<n;i++){
+        //     cout<<rightMax[i]<<" ";
+        // }
+        // cout<<"\n";
+        int maxarea=0;
+        for(int i=0;i<n;i++){
+            maxarea=max(maxarea, (rightMax[i]-leftMin[i]+1)*heights[i]);
+        }
+        return maxarea;
+    }
+};
