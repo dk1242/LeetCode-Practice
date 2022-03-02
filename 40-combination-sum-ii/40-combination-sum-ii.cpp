c2f6714e7c9b89b06738a8241beb641fbed83248
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-03-02 15:14:16 +0530

    Time: 7 ms (70.44%), Space: 10.7 MB (68.49%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    void backtrack(vector<int>&res1, vector<int>&candidates, int target, set<vector<int>>&ans, int start){
+    void backtrack(vector<int>&res1, vector<int>&candidates, int target, vector<vector<int>>&ans, int start){
         if(target==0){
             // cout<<"@";
-            vector<int>temp=res1;
-            sort(temp.begin(), temp.end());
-            ans.insert(temp);
+            // vector<int>temp=res1;
+            // sort(temp.begin(), temp.end());
+            ans.push_back(res1);
             // res1.clear();
             return;
         }
@@ -25,15 +25,15 @@ public:
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        set<vector<int>>ans;
+        // set<vector<int>>ans;
         vector<vector<int>>res;
         vector<int>res1;
         // cout<<candidates.size();
         sort(candidates.begin(), candidates.end());
-        backtrack(res1, candidates, target, ans, 0);
-        for(auto it:ans){
-            res.push_back(it);
-        }
+        backtrack(res1, candidates, target, res, 0);
+        // for(auto it:ans){
+        //     res.push_back(it);
+        // }
         return res;
     }
 };
