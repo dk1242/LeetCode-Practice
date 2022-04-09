20c3a72ac8029e3f90d14efd8d7023584833803b
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-04-09 11:30:56 +0530

    Time: 28 ms (28.24%), Space: 13.7 MB (41.62%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int, int>mp;
-        for(auto it:nums){
-            mp[it]++;
+        unordered_map<int,int> m;
+        for(auto x: nums) m[x]++;
+        priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+        for(auto x: m) {
+            pq.push({x.second, x.first});
+            if(pq.size()>k)
+                pq.pop();
         }
-        vector<int>res;
-        vector<pair<int, int>>vp;
-        for(auto it:mp){
-            vp.push_back({it.second, it.first});
+        vector<int> ans;
+        for(int i = 0; i < k; i++) {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
-        sort(vp.rbegin(), vp.rend());
-        for(auto &it:vp){
-            // cout<<it.first<<it.second<<" ";
-            if(k--){
-                res.push_back(it.second);
-            }else break;
-        }
-        return res;
+        return ans;
+        // map<int, int>mp;
+        // for(auto it:nums){
+        //     mp[it]++;
+        // }
+        // vector<int>res;
+        // vector<pair<int, int>>vp;
+        // for(auto it:mp){
+        //     vp.push_back({it.second, it.first});
+        // }
+        // sort(vp.rbegin(), vp.rend());
+        // for(auto &it:vp){
+        //     // cout<<it.first<<it.second<<" ";
+        //     if(k--){
+        //         res.push_back(it.second);
+        //     }else break;
+        // }
+        // return res;
     }
 };
