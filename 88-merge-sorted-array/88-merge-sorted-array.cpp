d0818a2eb50a1450bb10915eb04594782f24b637
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-12 11:57:37 +0530

    Time: 3 ms (70.50%), Space: 9.1 MB (73.24%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int j=m-1, k=n-1, i=0;
         for(i=n+m-1;k>=0 && j>=0 && i>=0;i--){
-            cout<<i;
+            // cout<<i;
             if(nums1[j]<=nums2[k]){
                 nums1[i]=nums2[k--];
             }else{
