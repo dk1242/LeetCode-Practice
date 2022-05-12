de300990b05ad67aa673731d7bd179ff792fd109
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-12 09:50:46 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (71.00%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int s=0,e=n-1;
+        for(int i=0;i<=e;){
+            if(nums[i]==0){
+                swap(nums[s++], nums[i++]);
+                // i--;
+            }else if(nums[i]==2){
+                swap(nums[e--], nums[i]);
+                // if(nums[i]!=0)
+                //     i--;
+                // i--;
+            }else i++;
+            // for(int j=0;j<n;j++)cout<<nums[j]<<" ";
+            // cout<<"\n";
+        }
+    }
+};
