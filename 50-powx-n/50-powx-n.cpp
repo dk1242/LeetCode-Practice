d1d846cb3643550a82d80f8d89d5ec8d1c97d3bc
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-13 12:46:14 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.47%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     double myPow(double x, long long n) {
-      double res=1.0;
+        if(x==1 || x==0)
+            return x;
         int f=0;
         if(n<0){
-            n*=-1;
             f=1;
+            n*=-1;
         }
+        double res=1.0;
         while(n>0){
             if(n&1){
                 res*=x;
@@ -14,7 +16,7 @@ public:
             n=n>>1;
             x*=x;
         }
-        if(f==1)
+        if(f)
             return 1/res;
         return res;
     }
