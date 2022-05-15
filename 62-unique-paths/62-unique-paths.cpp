832042aa4e82bebcd1eeb9b742479a6fee0b6398
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-15 13:16:30 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (44.87%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    unordered_map<string, int>mp;
-    int totalPaths(vector<vector<int>>&grid, int i, int j){
-        if(i>=grid.size() || j>=grid[0].size() || grid[i][j]!=0)
-            return 0;
-        string key="";
-        key=to_string(i)+"-"+to_string(j);
-        // cout<<key;
-        if(mp[key])
-            return mp[key];
-        if(i==grid.size()-1 && j==grid[0].size()-1)
-            return mp[key]=1;
-        grid[i][j]=1;
-        int ans=totalPaths(grid, i+1, j)+totalPaths(grid, i, j+1);
-        grid[i][j]=0;
-        return mp[key]=ans;
-    }
+    // unordered_map<string, int>mp;
+    // int totalPaths(vector<vector<int>>&grid, int i, int j){
+    //     if(i>=grid.size() || j>=grid[0].size() || grid[i][j]!=0)
+    //         return 0;
+    //     string key="";
+    //     key=to_string(i)+"-"+to_string(j);
+    //     // cout<<key;
+    //     if(mp[key])
+    //         return mp[key];
+    //     if(i==grid.size()-1 && j==grid[0].size()-1)
+    //         return mp[key]=1;
+    //     grid[i][j]=1;
+    //     int ans=totalPaths(grid, i+1, j)+totalPaths(grid, i, j+1);
+    //     grid[i][j]=0;
+    //     return mp[key]=ans;
+    // }
     int uniquePaths(int m, int n) {
         vector<vector<int>>grid(m, vector<int>(n, 0));
-        int ans=0;
-        ans=totalPaths(grid, 0, 0);
-        return ans;
+        // int ans=0;
+        // ans=totalPaths(grid, 0, 0);
+        // return ans;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0){
+                    grid[i][j]=1;
+                }else{
+                    grid[i][j]=grid[i-1][j]+grid[i][j-1];
+                }
+            }
+        }
+        return grid[m-1][n-1];
     }
 };
