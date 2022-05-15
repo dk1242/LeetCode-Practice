d77668b9c85b9128c80fa6be76bf8d89314f7739
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-15 13:13:06 +0530

    Time: 19 ms (5.72%), Space: 9.3 MB (26.45%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    int findUniquePaths(int i, int j, int m, int n, unordered_map<string, int> &mp){
-        string key=to_string(i)+","+to_string(j);
-        if(mp.find(key) != mp.end())
-            return mp[key];
-        if(i==m || j==n){
-            return mp[key]=1;
-        }
-        if(i>m || j>n)
-            return mp[key]=0;
-        return mp[key]=findUniquePaths(i+1, j, m, n, mp)+findUniquePaths(i, j+1, m, n, mp);
-        
-    }
-    
     unordered_map<string, int>mp;
+    int totalPaths(vector<vector<int>>&grid, int i, int j){
+        if(i>=grid.size() || j>=grid[0].size() || grid[i][j]!=0)
+            return 0;
+        string key="";
+        key=to_string(i)+"-"+to_string(j);
+        // cout<<key;
+        if(mp[key])
+            return mp[key];
+        if(i==grid.size()-1 && j==grid[0].size()-1)
+            return mp[key]=1;
+        grid[i][j]=1;
+        int ans=totalPaths(grid, i+1, j)+totalPaths(grid, i, j+1);
+        grid[i][j]=0;
+        return mp[key]=ans;
+    }
     int uniquePaths(int m, int n) {
-        return findUniquePaths(1, 1, m, n, mp);
-        // string key=to_string(m)+","+to_string(n);
-        // if(mp.find(key) != mp.end())
-        //     return mp[key];
-        // if(m==1 || n==1){
-        //     return mp[key]=1;
-        // }
-        // // if(m<=0 || n<=0)
-        // //     return mp[key]=0;
-        // return mp[key]=uniquePaths(m-1, n)+uniquePaths(m, n-1);
+        vector<vector<int>>grid(m, vector<int>(n, 0));
+        int ans=0;
+        ans=totalPaths(grid, 0, 0);
+        return ans;
     }
 };
