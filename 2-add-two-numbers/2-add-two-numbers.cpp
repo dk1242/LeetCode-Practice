93e4db7f0318ad1b8736b9eb3393fe11f823c083
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-17 09:32:40 +0530

    Time: 63 ms (22.63%), Space: 71.5 MB (12.15%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,22 +11,65 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* res=new ListNode(0);
-        ListNode *p=l1, *q=l2, *curr=res;
-        int carry=0,x,y,sum=0;
-        while(p!=NULL || q!=NULL){
-            x=p!=NULL?p->val:0;
-            y=q!=NULL?q->val:0;
-            sum=x+y+carry;
-            carry=sum/10;
-            curr->next=new ListNode(sum%10);
-            curr=curr->next;
-            if(p)p=p->next;
-            if(q)q=q->next;
+        ListNode* res=new ListNode();
+        ListNode* temp=res;
+        int sum=0, carry=0;
+        while(l1 && l2){
+            sum=l1->val+l2->val+carry;
+            
+            if(sum>=10){
+                temp->next=new ListNode(sum%10);
+                temp=temp->next;
+                carry=sum/10;
+            }
+            else {
+                temp->next=new ListNode(sum);
+                temp=temp->next;
+                carry=0;
+            }
+            l1=l1->next;
+            l2=l2->next;
         }
-        if(carry>0){
-            curr->next=new ListNode(carry);
+        if(l1){
+            while(l1 && carry){
+                sum=l1->val+carry;
+                if(sum>=10){
+                    temp->next=new ListNode(sum%10);
+                    temp=temp->next;
+                    carry=sum/10;
+                }else{
+                    temp->next=new ListNode(sum);
+                    temp=temp->next;
+                    carry=0;
+                }
+                l1=l1->next;
+            }
+            if(l1)temp->next=l1;
+            if(carry){
+                temp->next=new ListNode(carry);
+            }
+        }else if(l2){
+            while(l2 && carry){
+                sum=l2->val+carry;
+                if(sum>=10){
+                    temp->next=new ListNode(sum%10);
+                    temp=temp->next;
+                    carry=sum/10;
+                }else{
+                    temp->next=new ListNode(sum);
+                    temp=temp->next;
+                    carry=0;
+                }
+                l2=l2->next;
+            }
+            if(l2)temp->next=l2;
+            if(carry){
+                temp->next=new ListNode(carry);
+            }
+        }else if(carry){
+            temp->next=new ListNode(carry);
         }
+        
         return res->next;
     }
 };
