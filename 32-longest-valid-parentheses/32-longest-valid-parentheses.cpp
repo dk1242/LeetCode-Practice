f70c61128ec612b2833e92c5366ea32d5df9ceb3
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-24 17:52:53 +0530

    Time: 15 ms (10.35%), Space: 7.3 MB (67.93%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -3,15 +3,27 @@ public:
     int longestValidParentheses(string s) {
         int ans=0;
         int n=s.length();
-        vector<int>dp(n, 0);
-        for(int i=1;i<n;i++){
-            if(s[i]==')'){
-                if(s[i]=='('){
-                    dp[i]=(i>=2 ? dp[i-2] : 0)+2;
-                }else if(i-dp[i-1]>0 && s[i-dp[i-1]-1]=='('){
-                    dp[i]=dp[i-1]+((i-dp[i-1])>=2?dp[i-dp[i-1]-2]:0)+2;
-                }
-                ans=max(ans, dp[i]);
+        // vector<int>dp(n, 0);
+        // for(int i=1;i<n;i++){
+        //     if(s[i]==')'){
+        //         if(s[i]=='('){
+        //             dp[i]=(i>=2 ? dp[i-2] : 0)+2;
+        //         }else if(i-dp[i-1]>0 && s[i-dp[i-1]-1]=='('){
+        //             dp[i]=dp[i-1]+((i-dp[i-1])>=2?dp[i-dp[i-1]-2]:0)+2;
+        //         }
+        //         ans=max(ans, dp[i]);
+        //     }
+        // }
+        stack<int>st;
+        st.push(-1);
+        for(int i=0;i<n;i++){
+            if(s[i]=='(')
+                st.push(i);
+            else{
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }else ans=max(ans, i-st.top());
             }
         }
         return ans;
