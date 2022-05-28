840fec706e9899def8d07a7ec4427c0eaa40c1f3
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-28 08:53:25 +0530

    Time: 26 ms (55.22%), Space: 17.9 MB (93.84%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -3,10 +3,9 @@ public:
     int missingNumber(vector<int>& nums) {
         int ans=nums[0], n=nums.size();
         for(int i=1;i<n;i++){
-            ans^=nums[i];
+            ans+=nums[i];
         }
-        for(int i=0;i<=n;i++)
-            ans^=i;
+        ans=(n*(n+1)/2)-ans;
         return ans;
     }
 };
