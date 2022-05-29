742dfa7ffba290e66ec76a41c59ce558096263e3
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-29 21:34:56 +0530

    Time: 71 ms (56.81%), Space: 15.5 MB (89.46%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -3,15 +3,15 @@ public:
     int maxProduct(vector<string>& words) {
         int n=words.size();
         int ans=0, f=0;
+        vector<int>mask(n, 0);
         for(int i=0;i<n;i++){
-            sort(words[i].begin(), words[i].end());
+            for(auto it:words[i])
+                mask[i]=mask[i]|(1<<(it-'a'));
+            // cout<<mask[i]<<" "<<bitset<26>(mask[i]).to_string()<<" ";
         }
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                string intersect="";
-                set_intersection(words[i].begin(), words[i].end(), words[j].begin(), words[j].end(), std::back_inserter(intersect));
-                // cout<<intersect<<"@ ";
-                if(intersect.length()==0){
+                if((mask[i]&mask[j])==0){
                     int l1=words[i].length(), l2=words[j].length();
                     // cout<<words[i]<<" "<<words[j]<<" ";
                     ans=max(ans, l1*l2);
