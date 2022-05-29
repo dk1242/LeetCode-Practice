f5af13db3f8f274648db94fa4f0129d2b8384df0
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-29 16:42:20 +0530

    Time: 4 ms, Memory: 16.8 MB - LeetHub

--- symmetric-tree/symmetric-tree.cpp
+++ symmetric-tree/symmetric-tree.cpp
@@ -22,6 +22,30 @@ public:
     bool isSymmetric(TreeNode* root) {
         if(root==NULL)
             return false;
-        return checkSymmetric(root->left, root->right);
+        // return checkSymmetric(root->left, root->right);
+        queue<TreeNode*>q1, q2;
+        TreeNode* left, *right;
+        q1.push(root->left);
+        q2.push(root->right);
+        while(!q1.empty() && !q2.empty()){
+            left=q1.front();
+            right=q2.front();
+            q1.pop();
+            q2.pop();
+            if(left==NULL && right==NULL){
+                continue;
+                
+            }
+            if(!left || !right)
+                return false;
+            if(left->val != right->val)
+                return false;
+            q1.push(left->left);
+            q1.push(left->right);
+            q2.push(right->right);
+            q2.push(right->left);
+            
+        }
+        return true;
     }
 };
