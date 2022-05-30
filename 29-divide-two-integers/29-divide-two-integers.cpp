8e67464ace8df5116a8ebd5c17e718838852264c
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-05-30 14:59:07 +0530

    Time: 7 ms (27.04%), Space: 6 MB (30.45%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
     int divide(int dd, int ds) {
-        // bool isNeg=(dd<0 && ds>0) || (dd>0 && ds<0);
-        // long long res=0, temp, count;
-        // long long dd1=abs(dd), ds1=abs(ds);
-        // while(dd1>=ds1){
-        //     temp=ds1, count=1;
-        //     // cout<<temp;
-        //     while(temp<=dd1){
-        //         temp*=2;
-        //         count*=2;
-        //     }
-        //     res+=count/2;
-        //     dd1-=temp/2;
-        // }
-        // if(isNeg)
-        //     res*=-1;
-        // if(res>INT_MAX)
-        //     res=INT_MAX;
-        // return res;
-        long long t=0, q=0, n=labs(dd), m=labs(ds);
-        int sign = (dd<0) ^ (ds<0) ? -1 : 1;
-        for(long long i=31;i>=0;i--)
-        {
-			//(m<<i) = m * (2^i)
-            if(t + (m<<i) <= n)
-            {
-                t += (m<<i);
-				
-				//since 2^i here can be large for int so we typecast it in long long to prevent overflow 
-                q += (1LL<<i);
+        bool isNeg=(dd<0 && ds>0) || (dd>0 && ds<0);
+        long long res=0, temp, count;
+        long long dd1=abs(dd), ds1=abs(ds);
+        while(dd1>=ds1){
+            temp=ds1, count=1;
+            // cout<<temp;
+            while(temp*2<=dd1){
+                temp*=2;
+                count*=2;
             }
+            res+=count;
+            dd1-=temp;
         }
-        cout<<q;
-        if(sign<0) q=-q;
-        return ((q>INT_MAX) or (q<INT_MIN)) ? INT_MAX : q;
+        if(isNeg)
+            res*=-1;
+        if(res>INT_MAX)
+            res=INT_MAX;
+        return res;
     }
 };
