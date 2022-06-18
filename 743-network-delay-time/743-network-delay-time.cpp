f9b2ecda822853ff070f72ee8ca56d3ce7625302
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-06-18 11:14:21 +0530

    Time: 145 ms (73.17%), Space: 43.6 MB (27.37%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        queue<int>q;
+        unordered_map<int, vector<pair<int, int>>>graph;
+        for(auto it:times){
+            graph[it[0]].push_back({it[1], it[2]});
+        }
+        q.push(k);
         vector<int>dist(n+1, INT_MAX);
         dist[k]=0;
-        for(int i=0;i<n;i++){
-            for(auto it:times){
-                int u=it[0], v=it[1], w=it[2];
-                if(dist[u]!=INT_MAX && dist[v]>dist[u]+w){
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            for(auto [v, w]:graph[u]){
+                if(dist[v]>dist[u]+w){
                     dist[v]=dist[u]+w;
+                    q.push(v);
                 }
             }
         }
+        // for(int i=0;i<n;i++){
+        //     for(auto it:times){
+        //         int u=it[0], v=it[1], w=it[2];
+        //         if(dist[u]!=INT_MAX && dist[v]>dist[u]+w){
+        //             dist[v]=dist[u]+w;
+        //         }
+        //     }
+        // }
+        // for(auto it:dist)
+        //     cout<<it<<" ";
+        // cout<<endl;
         int ans=0;
         for(int i=1;i<=n;i++){
             ans=max(ans, dist[i]);
