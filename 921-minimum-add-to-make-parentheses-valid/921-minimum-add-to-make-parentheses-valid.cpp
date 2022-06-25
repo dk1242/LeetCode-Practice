bdcc2d5fa512dd947d8880f1954c86f4126c4620
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-06-25 14:08:02 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (83.07%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char>st;
+        // stack<char>st;
+        int open=0,close=0;
         for(char& c: s) {
             if(c=='('){
-                st.push(c);
-            }else if(!st.empty() && st.top()=='('){
-                st.pop();
-            }else st.push(c);
+                // st.push(c);
+                open++;
+            }
+            // else if(!st.empty() && st.top()=='('){
+            //     st.pop();
+            // }else st.push(c);
+            else if(open>0){
+                open--;
+            }else close++;
         }
-        return st.size();
+        // return st.size();
+        return open+close;
     }
 };
