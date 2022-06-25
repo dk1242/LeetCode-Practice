a5b2d7945746829477903c279dd1cd1be27982d1
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-06-25 09:53:33 +0530

    Time: 32 ms (78.06%), Space: 27.1 MB (12.14%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -3,10 +3,19 @@ public:
     bool checkPossibility(vector<int>& nums) {
         int n=nums.size();
         int c=0;
-        for(int i=1;i<n;i++){
-            if(nums[i-1]>nums[i])
-                if(c++ || i>1 && i<n-1 && nums[i-2]>nums[i] && nums[i+1]<nums[i-1])
+        for(int i=1, prev=nums[0];i<n;i++){
+            // cout<<prev<<" "<<nums[i-1]<<"\n";
+            if(prev>nums[i]){
+                if(c++)
                     return false;
+                // if(i-2>=0 && nums[i-2]>nums[i])
+                //     nums[i]=nums[i-1];
+                // else nums[i-1]=nums[i];
+                
+                if(i-2>=0 && nums[i-2]>nums[i])
+                    continue;
+            }
+            prev=nums[i];
         }
         return true;
     }
