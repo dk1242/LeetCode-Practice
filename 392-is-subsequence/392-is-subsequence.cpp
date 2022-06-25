88ef1103ce4e474aa4502e5468b38be13c8b9fad
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-06-25 13:41:54 +0530

    Time: 3 ms (54.94%), Space: 6.4 MB (31.78%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int sl=s.length(), tl=t.length();
-        int i=0,j=0;
-        if(sl==0){
-            return true;
-        }
-        while(i<sl && j<tl){
+        int n=s.length(), m=t.length();
+        int i=0, j=0;
+        while(i<n && j<m){
             if(s[i]==t[j]){
-                
-                if(i==sl-1){
-                    return true;
-                }
-                i++,j++;
+                i++, j++;
             }else{
                 j++;
             }
         }
-        return false;
+        if(i<n)
+            return false;
+        return true;
     }
 };
