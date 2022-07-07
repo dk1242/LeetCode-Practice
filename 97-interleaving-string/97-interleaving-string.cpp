a9f72e8b9f2e82336bc6fc2033cf755a179fb5e5
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-07-07 18:17:06 +0530

    Time: 53 ms (22.49%), Space: 31.7 MB (17.55%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
+    bool dfs(string s1, string s2, string s3, int i, int j, int k, vector<vector<int>>&dp){
+        if(i<0 && j<0 && k<0)
+            return true;
+        if(i>=0 && j>=0 && dp[i][j]!=-1)
+            return dp[i][j];
+        if(i>=0 && s1[i]==s3[k] && j>=0 && s2[j]==s3[k])
+            return dp[i][j]=dfs(s1, s2, s3, i-1, j, k-1, dp) | dfs(s1, s2, s3, i, j-1, k-1, dp);
+        else if(i>=0 && s1[i]==s3[k])
+            return dfs(s1, s2, s3, i-1, j, k-1, dp);
+        else if(j>=0 && s2[j]==s3[k])
+            return dfs(s1, s2, s3, i, j-1, k-1, dp);
+        else return false;
+    }
     bool isInterleave(string s1, string s2, string s3) {
         int n=s1.length(), m=s2.length(), l=s3.length();
         if(n+m != l)
             return false;
-        vector<vector<int>>vis(n+1, vector<int>(m+1, 0));
-        queue<vector<int>>q;
-        q.push({0,0});
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            if(it[0]==n && it[1]==m)return true;
-            if(vis[it[0]][it[1]])continue;
-            if(it[0]<n && s1[it[0]]==s3[it[0]+it[1]])
-                q.push({it[0]+1, it[1]});
-            if(it[1]<m && s2[it[1]]==s3[it[0]+it[1]])
-                q.push({it[0], it[1]+1});
-            vis[it[0]][it[1]]=1;
-        }
-        return false;
+        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+        return dfs(s1, s2, s3, n-1, m-1, l-1, dp);
     }
 };
