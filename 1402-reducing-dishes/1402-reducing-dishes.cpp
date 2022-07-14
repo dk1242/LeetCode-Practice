e4ca2f87e0cd9fdd813a3e51efe608c05365d882
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-07-14 22:39:55 +0530

    Time: 8 ms (61.41%), Space: 8 MB (92.88%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -11,8 +11,23 @@ public:
         return dp[i][time]=max({0, ans1, ans2});
     }
     int maxSatisfaction(vector<int>& sats) {
-        dp.resize(sats.size()+1, vector<int>(sats.size()+1, -1));
-        sort(sats.begin(), sats.end());
-        return dfs(sats, 0, 1);
+        // dp.resize(sats.size()+1, vector<int>(sats.size()+1, -1));
+        sort(sats.rbegin(), sats.rend());
+        // return dfs(sats, 0, 1);
+        int n=sats.size();
+        int sum=0;
+        vector<int>pre;
+        for(int i=0;i<n;i++){
+            sum+=sats[i];
+            if(sum<0)
+                break;
+            pre.push_back(sats[i]);
+        }
+        reverse(pre.begin(), pre.end());
+        sum=0;
+        for(int i=0;i<pre.size(); i++){
+            sum+=(i+1)*pre[i];
+        }
+        return sum;
     }
 };
