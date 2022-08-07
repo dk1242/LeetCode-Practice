eb213039099b10e09fa5d213e06e51942d1e1abc
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-08-07 11:21:32 +0530

    Time: 15 ms (53.96%), Space: 11.9 MB (46.28%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     void f(vector<int>&cand, int tar, int ind, vector<vector<int>>&res, vector<int>&temp){
-        if(ind==cand.size()){
-            if(tar==0){
-                res.push_back(temp);
-                // return;
-            }
-            return;
-        }
-        if(tar<0)
-            return;
+        // if(ind==cand.size()){
+        //     if(tar==0){
+        //         res.push_back(temp);
+        //         // return;
+        //     }
+        //     return;
+        // }
         if(tar==0){
             res.push_back(temp);
             return;
         }
-        f(cand, tar, ind+1, res, temp);
-        temp.push_back(cand[ind]);
-        f(cand, tar-cand[ind], ind, res, temp);
-        temp.pop_back();
+        if(tar<0)
+            return;
+        
+        // f(cand, tar, ind+1, res, temp);
+        for(int i=ind;i<cand.size();i++){
+            temp.push_back(cand[i]);
+            f(cand, tar-cand[i], i, res, temp);
+            temp.pop_back();        
+        }
     }
     vector<vector<int>> combinationSum(vector<int>& cand, int tar) {
         vector<vector<int>>res;
         vector<int>temp;
+        vector<vector<int>>memo(cand.size(), vector<int>(tar, -1));
         f(cand, tar, 0, res, temp);
         return res;
     }
