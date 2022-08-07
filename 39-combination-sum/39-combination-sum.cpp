92bde91d70eba7fb5e102fec8af09d576721a69c
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-08-07 11:12:27 +0530

    Time: 26 ms (36.88%), Space: 17.4 MB (36.77%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    void backtrack(vector<int>&res1, vector<int>&candidates, int target, vector<vector<int>>&ans, int start){
-        if(target==0){
-            // vector<int>temp=res1;
-            // sort(temp.begin(), temp.end());
-            ans.push_back(res1);
-            // res1.clear();
+    void f(vector<int>&cand, int tar, int ind, vector<vector<int>>&res, vector<int>&temp){
+        if(ind==cand.size()){
+            if(tar==0){
+                res.push_back(temp);
+                // return;
+            }
             return;
         }
-        if(target<0){
-            // res1.pop_back();
+        if(tar<0)
+            return;
+        if(tar==0){
+            res.push_back(temp);
             return;
         }
-        int n=candidates.size();
-        for(int i=start;i<n;i++){
-            res1.push_back(candidates[i]);
-            backtrack(res1, candidates, target-candidates[i], ans, i);
-            res1.pop_back();
-        }
+        f(cand, tar, ind+1, res, temp);
+        temp.push_back(cand[ind]);
+        f(cand, tar-cand[ind], ind, res, temp);
+        temp.pop_back();
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        // set<vector<int>>res;
-        // sort(candidates.begin(), candidates.end());
-        vector<vector<int>>ans;
-        vector<int>res1;
-        backtrack(res1, candidates, target, ans, 0);
-        // for(auto it:res){
-        //     ans.push_back(it);
-        // }
-        return ans;
+    vector<vector<int>> combinationSum(vector<int>& cand, int tar) {
+        vector<vector<int>>res;
+        vector<int>temp;
+        f(cand, tar, 0, res, temp);
+        return res;
     }
 };
