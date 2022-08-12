3d0be4ca1d91e978104c6b1fd00e4258b4e35e0a
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-08-12 10:28:10 +0530

    Time: 12 ms (89.85%), Space: 11.7 MB (89.72%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(10002, 0);
+        int n=nums.size(), m=*max_element(nums.begin(), nums.end());
+        vector<int>dp(m+1, 0);
         for(int i=0;i<n;i++){
             dp[nums[i]]+=nums[i];
         }
-        for(int i=2;i<10002;i++){
+        for(int i=2;i<=m;i++){
             dp[i]=max(dp[i-1], dp[i-2]+dp[i]);
         }
-        return dp[10001];
+        return dp[m];
     }
 };
