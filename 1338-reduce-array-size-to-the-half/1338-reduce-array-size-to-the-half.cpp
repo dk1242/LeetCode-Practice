199fb74f0dcc8ec06a7b328431febc22beb25b7a
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-08-18 21:47:05 +0530

    Time: 530 ms (13.46%), Space: 80.2 MB (39.23%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        map<int, int>mp;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        }
+        vector<int>freq;
+        for(auto it:mp){
+            freq.push_back(it.second);
+        }
+        sort(freq.rbegin(), freq.rend());
+        int ans=0, curr=0;
+        for(int i=0;i<freq.size();i++){
+            curr+=freq[i];
+            ans++;
+            if(curr>=n/2){
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
