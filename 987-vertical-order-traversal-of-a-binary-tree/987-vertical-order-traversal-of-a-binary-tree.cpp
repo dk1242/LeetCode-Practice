e676e5fd5305ddffdb82955bde6eae6c7c86d84b
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-09-04 12:02:39 +0530

    Time: 8 ms (58.22%), Space: 15.7 MB (5.35%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,21 +11,21 @@
  */
 class Solution {
 public:
-    void bfs(TreeNode* node, map<int,map<int, multiset<int>>>&mp, int hd, int ht){
+    void dfs(TreeNode* node, map<int,map<int, multiset<int>>>&mp, int hd, int ht){
       if(node==NULL){
           return;
       }
       mp[hd][ht].insert(node->val);
       if(node->left)
-      bfs(node->left, mp, hd-1, ht+1);
+      dfs(node->left, mp, hd-1, ht+1);
       if(node->right)
-      bfs(node->right, mp, hd+1, ht+1);
+      dfs(node->right, mp, hd+1, ht+1);
     }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         map<int, map<int, multiset<int>>>mp;
         vector<vector<int>>res;
         int count=0;
-        bfs(root, mp,0, count);
+        dfs(root, mp,0, count);
         for(auto it:mp){
             // cout<<it.first<<" ";
             vector<int>col;
