f93901327e7f541af5ffe1b0473a85e4c989fbc5
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-09-24 09:35:06 +0530

    Time: 23 ms (47.01%), Space: 33.7 MB (40.59%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -11,24 +11,30 @@
  */
 class Solution {
 public:
-    void findPathSum(TreeNode* root, int targetSum, int currSum, vector<vector<int>>&res, vector<int>&ans){
+    void findPathSum(TreeNode* root, int targetSum, int currSum, vector<vector<int>>&res, vector<int>ans){
         if(root==NULL)
-            return ;
-        ans.push_back(root->val);
-        if(!root->left && !root->right && currSum+root->val==targetSum){
+            return;
+        if(!root->left && !root->right && currSum==targetSum){
+            ans.push_back(root->val);
             res.push_back(ans);
-            
+            ans.clear();
+            return;
         }
-        findPathSum(root->left, targetSum, currSum+root->val, res, ans);
-        findPathSum(root->right, targetSum, currSum+root->val, res, ans);
-        ans.pop_back();
+        ans.push_back(root->val);
+        if(root->left){
+            findPathSum(root->left, targetSum, currSum+root->left->val, res, ans);
+        }
+        if(root->right){
+            findPathSum(root->right, targetSum, currSum+root->right->val, res, ans);
+        }
+        // return;
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>>res;
         if(root==NULL)
             return res;
         vector<int>ans;
-        findPathSum(root, targetSum, 0, res, ans);
+       findPathSum(root, targetSum, root->val, res, ans);
         return res;
     }
 };
