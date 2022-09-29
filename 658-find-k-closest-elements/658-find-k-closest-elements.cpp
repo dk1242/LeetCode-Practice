571db796d828b2504a4c96b9e60361630d2c5b21
Dhruval Kushwaha <46108847+dk1242@users.noreply.github.com>
2022-09-29 13:03:21 +0530

    Time: 115 ms (47.49%), Space: 31.1 MB (86.01%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<pair<int, int>>vp;
-        for(int i=0;i<arr.size();i++){
-            vp.push_back({abs(arr[i]-x), arr[i]});
+        // vector<pair<int, int>>vp;
+        // for(int i=0;i<arr.size();i++){
+        //     vp.push_back({abs(arr[i]-x), arr[i]});
+        // }
+        // sort(vp.begin(), vp.end());
+        // vector<int>ans(k,0);
+        // for(int i=0;i<k;i++){
+        //     ans[i]=vp[i].second;
+        // }
+        // sort(ans.begin(), ans.end());
+        // return ans;
+        int lo=0, hi=arr.size()-k, mid;
+        while(lo<hi){
+            mid=lo+(hi-lo)/2;
+            if(x-arr[mid]>arr[mid+k]-x){
+                lo=mid+1;
+            }else hi=mid;
         }
-        sort(vp.begin(), vp.end());
-        vector<int>ans(k,0);
-        for(int i=0;i<k;i++){
-            ans[i]=vp[i].second;
-        }
-        sort(ans.begin(), ans.end());
-        return ans;
+        // cout<<lo<<hi<<"\n";
+        return vector<int>(arr.begin()+lo, arr.begin()+lo+k);
     }
 };
